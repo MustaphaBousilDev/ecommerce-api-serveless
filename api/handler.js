8b668ed00abc4e2eb847fb4e45030306050124ff
MustaphaBousilDev <bousilmustapha@gmail.com>
2025-07-05 22:35:05 +0100

    get request login from middleware login

--- api/handler.js
+++ api/handler.js
@@ -23,6 +23,7 @@ const {
   logPerformance, 
   logSecurity 
 } = require('./utils/logger');
+const { requestLoggin } = require("./middlewares/logger");
 
 // X-Ray Configuration with error handling
 let AWSXRay;
@@ -104,40 +105,6 @@ const closeSubsegment = (subsegment) => {
 };
 
 // Your Custom Request Logging Middleware with X-Ray Integration
-const requestLoggin = (req, res, next) => {
-  const startTime = Date.now();
-  const requestId = uuidv4();
-  
-  req.requestId = requestId;
-  req.startTime = startTime;
-  
-  logRequest(req, requestId);
-  
-  // Override res.json to log response
-  const originalJSON = res.json;
-  res.json = function(body) {
-    const duration = Date.now() - startTime;
-    logResponse(req, res, requestId, duration);
-    
-    if (duration > 500) {
-      logPerformance(`${req.method} ${req.path}`, duration, requestId, req.user?.userId);
-    }
-    
-    return originalJSON.call(this, body);
-  };
-  
-  res.on('finish', () => {
-    if (res.statusCode >= 400) {
-      logError(new Error(`HTTP ${res.statusCode}`), requestId, req.user?.userId, {
-        method: req.method,
-        path: req.path,
-        statusCode: res.statusCode
-      });
-    }
-  });
-  
-  next();
-};
 
 // Your Custom Error Logging Middleware
 const errorLoggin = (err, req, res, next) => {
